4f585778289f2a0592405eaad56690bdec5236e9
Erik Kristensen <erik@erikkristensen.com>
2021-04-15 09:14:40 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -2,7 +2,15 @@ resource "random_id" "test" {
   byte_length = 16
 }
 
+resource "random_id" "test2" {
+  byte_length = 64
+}
+
 output "testing" {
   value = random_id.test.hex
+}
+
+output "testing2" {
+  value = random_id.test2.hex
 }
 
